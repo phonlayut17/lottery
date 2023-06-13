4a4dfc54b5e58918b878d5163e19793c36ca5757
phonlayut88 <phonlayut.sop@axonstech.com>
2023-06-13 20:44:20 +0700

    [Added] sum for dialog

--- src/components/History.js
+++ src/components/History.js
@@ -10,8 +10,13 @@ const History = (props) => {
     const [showMessage, setShowMessage] = useState('');
     const [id, setId] = useState('');
     const [data, setData] = useState([]);
+    const [detail, setDetail] = useState([]);
+    const [lottery, setLottery] = useState('');
+    const [date, setDate] = useState('');
     const [showButton, setShowButton] = useState(true);
     const [showSpinner, setShowSpinner] = useState(false);
+    const [showDetail, setShowDetail] = useState(false);
+    const [totalPrice, setTotalPrice] = useState('');
 
     const handleCloseSpinner = () => {
         setShowSpinner(false);
@@ -19,6 +24,14 @@ const History = (props) => {
 
     const handleShowSpinner = () => {
         setShowSpinner(true);
+    };
+
+    const handleCloseDetail = () => {
+        setShowDetail(false);
+    };
+
+    const handleShowDetail = () => {
+        setShowDetail(true);
     };
 
     const handleCloseModal = () => {
@@ -34,6 +47,7 @@ const History = (props) => {
     };
 
     const getData = async () => {
+        handleShowSpinner();
         try {
             const dataRes = await axios.post('https://luckynumber-777-hhbuvnb5vq-uc.a.run.app/get-list-by-user', {
                 user: props.user
@@ -43,6 +57,29 @@ const History = (props) => {
         } catch (error) {
             console.log(error);
         }
+        handleCloseSpinner();
+    };
+
+    const getDataById = async (id) => {
+        handleShowSpinner();
+        try {
+            const dataRes = await axios.post('https://luckynumber-777-hhbuvnb5vq-uc.a.run.app/get-data-by-id', {
+                id: id
+            });
+            if (dataRes.data.success) {
+                setLottery(dataRes.data.lot_type);
+                cvDate(dataRes.data.date);
+                setDetail(dataRes.data.data);
+                setTotalPrice(dataRes.data.total);
+                handleShowDetail();
+            } else {
+                console.log(dataRes.data.message);
+            }
+            console.log(dataRes.data);
+        } catch (error) {
+            console.log(error);
+        }
+        handleCloseSpinner();
     };
 
     useEffect(() => {
@@ -67,7 +104,15 @@ const History = (props) => {
         }
     };
 
+    const cvDate = (dateR) => {
+        const dataDate = new Date(dateR);
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        const thaiDate = dataDate.toLocaleDateString("th-TH", options);
+        setDate(thaiDate);
+    };
+
     const isDataArray = Array.isArray(data);
+    const isDetailArray = Array.isArray(detail);
 
     return (
         <Col>
@@ -89,11 +134,11 @@ const History = (props) => {
                         </tr>
                     ) : isDataArray ? (
                         data.map((item, b) => (
-                            <tr key={b}>
+                            <tr key={b} onClick={() => getDataById(item.data_hdr_id)}>
                                 <td>{b + 1}</td>
                                 <td>{new Date(item.data_dtl_date).toLocaleDateString('th-TH')}</td>
                                 <td>{item.data_hdr_lot_type}</td>
-                                <td>{item.data_dtl_price}</td>
+                                <td>{item.data_hdr_total_price}</td>
                                 <td>{item.data_hdr_comment}</td>
                                 <td><IoTrashBinOutline size={20} style={{ color: '#D50000' }} onClick={() => handleShowModal(item.data_hdr_id)} /></td>
                             </tr>
@@ -128,6 +173,48 @@ const History = (props) => {
                     <br />
                     <h4>รอสักครู่...</h4>
                 </Modal.Body>
+            </Modal>
+            <Modal show={showDetail} onHide={handleCloseDetail} centered>
+                <Modal.Body>
+                    <h5>หวย{lottery} - {date}</h5>
+                    <table className="table table-striped">
+                        <thead className="text-center">
+                            <tr>
+                                <th scope="col"><h4>#</h4></th>
+                                <th scope="col"><h4>ประเภท @ หมายเลข</h4></th>
+                                <th scope="col"><h4>ราคา</h4></th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-center">
+                            {isDetailArray && detail.length === 0 ? (
+                                <tr>
+                                    <td colSpan="7">❌ ไม่มีข้อมูล ❌</td>
+                                </tr>
+                            ) : isDetailArray ? (
+                                detail.map((item, b) => (
+                                    <tr key={b}>
+                                        <td>{b + 1}</td>
+                                        <td>{item.data_dtl_type_price} @ {item.data_dtl_number}</td>
+                                        <td>{item.data_dtl_price}</td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="7">❌ ไม่มีข้อมูล ❌</td>
+                                </tr>
+                            )}
+                            <tr>
+                                <td colSpan="2">รวม {detail.length} รายการ</td>
+                                <td>{totalPrice} บาท</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseDetail}>
+                        ปิด
+                    </Button>
+                </Modal.Footer>
             </Modal>
         </Col >
     );
